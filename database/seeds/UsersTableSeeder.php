45344164b2a2209020fbd911a7496072fa39a77c
Xavier Meler <jmeler@xtec.cat>
2017-04-11 15:43:56 +0000

    Added basic seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,18 +14,63 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         
-    	$school = Entity::where('name','Institut de Sabadell')->first();
+    	$school = Entity::first();
 
 		User::create ([
-    		'name'=>'Xavi Meler',
-    		'nickname' =>'jmeler',
-    		'entity_id' => $school->id,
-    		'email' => 'jmeler@xtec.cat',
+            'name'=>'Sinapsi',
+            'nickname' =>'sinapsi',
+            'entity_id' => $school->id,
+            'email' => 'agora@xtec.cat',
+            'avatar' => '',
+            'role' =>'admin'
+        ]);
+
+        User::create ([
+            'name'=>'Xavi Meler',
+            'nickname' =>'jmeler',
+            'entity_id' => $school->id,
+            'email' => 'jmeler@xtec.cat',
             'avatar' => 'https://lh3.googleusercontent.com/-xsv0-7qtrMc/AAAAAAAAAAI/AAAAAAAAAAA/fevX2Yl5pxY/photo.jpg?sz=50',
             'role' =>'admin'
-    	]);
-        
-        factory(User::class)->times(200)->create();
+        ]);
+
+        User::create ([
+            'name'=>'Toni Ginard',
+            'nickname' =>'aginard',
+            'entity_id' => $school->id,
+            'email' => 'aginard@xtec.cat',
+            'avatar' => '',
+            'role' =>'admin'
+        ]);
+
+        User::create ([
+            'name'=>'Xavi Nieto',
+            'nickname' =>'xnieto2',
+            'entity_id' => $school->id,
+            'email' => 'xnieto2@xtec.cat',
+            'avatar' => '',
+            'role' =>'admin'
+        ]);
+
+        User::create ([
+            'name'=>'Sara Arjona',
+            'nickname' =>'sarjona',
+            'entity_id' => $school->id,
+            'email' => 'sarjona@xtec.cat',
+            'avatar' => '',
+            'role' =>'admin'
+        ]);
+
+        User::create ([
+            'name'=>'Monica Grau',
+            'nickname' =>'mgrau226',
+            'entity_id' => $school->id,
+            'email' => 'mgrau226@xtec.cat',
+            'avatar' => '',
+            'role' =>'admin'
+        ]);
+
+        //factory(User::class)->times(10)->create();
 
     }
 }
