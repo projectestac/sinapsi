08ff1d9716135121730818b236b94be95fa54f60
Joan Sala <jsala236@xtec.cat>
2018-04-18 17:21:52 +0200

    Updates the frontend localization messages
    * Updated messages for: Catalan, English and Spanish.

--- frontend/src/locales/messages.es.ts
+++ frontend/src/locales/messages.es.ts
@@ -2,105 +2,270 @@
 
 /** Translated messages */
 export const I18N_MESSAGES = {
-    "The school «<strong>{name}</strong>» has been deleted.":
-    "La escuela «<strong>{name}</strong>» ha sido eliminada.",
-
-    "Delete synapse":
-    "Suprime la sinapsi",
-
-    "Delete tag":
-    "Suprime la etiqueta",
-
-    "Synapse editor":
-    "Edición de sinapsis",
-
-    "This action cannot be undone.":
-    "Esta acción no puede deshacerse.",
-
-    "Share on Twitter":
-    "Compartir en Twitter",
-
-    "Delete comment":
-    "Suprime el comentario",
-
-    "Share on Facebook":
-    "Compartir en Facebook",
-
-    "Taxonomies":
-    "Taxonomías",
+    "About":
+    "Acerca de",
 
     "Advanced search":
     "Búsqueda avanzada",
 
-    "Disable user":
-    "Deshabilitar el usuario",
+    "All posts":
+    "Todos los artículos",
+
+    "Any time":
+    "En cualquier momento",
+
+    "Author editor":
+    "Edición de autores",
+
+    "Black":
+    "Negro",
+
+    "Blue":
+    "Azul",
+
+    "Cancel":
+    "Cancelar",
 
     "Catalogs":
     "Catálogos",
 
-    "Tags":
-    "Etiquetas",
+    "Commented":
+    "Comentados",
 
-    "The synapse «<strong>{name}</strong>» has been deleted.":
-    "La sinapsi «<strong>{name}</strong>» ha sido eliminada.",
+    "Commented articles":
+    "Artículos comentados",
 
-    "Do you want to delete «<strong>{name}</strong>»?":
-    "¿Seguro que quieres suprimir  «<strong>{name}</strong>»?",
+    "Create project":
+    "Crear proyecto",
 
-    "Schools":
-    "Escuelas",
+    "Create school":
+    "Crear centro",
 
-    "Author editor":
-    "Edición de editores",
+    "Create synapse":
+    "Crear sinapsis",
 
-    "The comment «{name}» has been deleted.":
-    "El comentario «{name}» ha sido eliminado.",
+    "Cyan":
+    "Cian",
 
-    "Projects":
-    "Proyectos",
-
-    "About":
-    "Acerca de",
-
-    "The user «<strong>{name}</strong>» has been successfully disabled.":
-    "El usuario «<strong>{name}</strong>» ha sido desactivado con éxito.",
-
-    "Your account has been temporarily disabled.":
-    "Tu cuenta se inhabilitó temporalmente.",
-
-    "Please, log in to like or favourite posts.":
-    "Por favor, entre para marcar los artículos como favoritos.",
-
-    "Table of contents":
-    "Tabla de contenidos",
-
-    "The project «<strong>{name}</strong>» has been deleted.":
-    "El proyecto «<strong>{name}</strong>» ha sido eliminado.",
-
-    "Share by Email":
-    "Compartir por correo electrónico",
+    "Default ordering":
+    "Ordenación predeterminada",
 
     "Delete":
     "Suprimir",
 
-    "The tag «<strong>{name}</strong>» has been deleted.":
-    "La etiqueta «<strong>{name}</strong>» ha sido eliminada.",
+    "Delete comment":
+    "Suprime el comentario",
 
-    "Web syndication":
-    "Sindicación web",
+    "Delete synapse":
+    "Suprime la sinapsis",
 
-    "Help":
-    "Ayuda",
+    "Delete tag":
+    "Suprime la etiqueta",
 
-    "Educators":
-    "Docentes",
+    "Disable user":
+    "Deshabilitar el usuario",
 
-    "Synapses":
-    "Sinapsis",
+    "Do you want to delete «<strong>{name}</strong>»?":
+    "¿Seguro que quieres suprimir  «<strong>{name}</strong>»?",
+
+    "Do you want to delete this block?":
+    "¿Quieres suprimir este bloque?",
 
     "Do you want to disable the user «<strong>{name}</strong>»?":
     "¿Seguro que quieres desactivar el usuario  «<strong>{name}</strong>»?",
 
-    "Cancel":
-    "Cancelar"
+    "Educators":
+    "Docentes",
+
+    "Everything":
+    "Todos",
+
+    "Favourited":
+    "Favoritos",
+
+    "Favourited articles":
+    "Artículos favoritos",
+
+    "Featured":
+    "Destacados",
+
+    "Featured articles":
+    "Artículos destacados",
+
+    "Gray":
+    "Gris",
+
+    "Green":
+    "Verde",
+
+    "Help":
+    "Ayuda",
+
+    "Liked":
+    "Votados",
+
+    "Liked articles":
+    "Artículos votados",
+
+    "Most commented":
+    "Más comentados",
+
+    "Most commented articles":
+    "Artículos más comentados",
+
+    "Most recent articles first":
+    "Los artículos más recientes primero",
+
+    "Most relevant results first":
+    "Los resultados más relevantes primero",
+
+    "Most voted":
+    "Más votados",
+
+    "Most voted articles":
+    "Artículos más votados",
+
+    "Municipalities":
+    "Municipios",
+
+    "Name of the project":
+    "Nombre del proyecto",
+
+    "Name of the school":
+    "Nombre del centro",
+
+    "Name of the synapse":
+    "Nombre de la sinapsis",
+
+    "No synapse exists for the tag «<strong>{name}</strong>».":
+    "No existe ninguna sinapsis para la etiqueta «<strong>{name}</strong>».",
+
+    "Oldest articles first":
+    "Los artículos más antiguos primero",
+
+    "Orange":
+    "Naranja",
+
+    "Pink":
+    "Rosa",
+
+    "Please enter a name for the new project.":
+    "Escriba un nombre para el nuevo proyecto.",
+
+    "Please enter a name for the new school.":
+    "Escriba un nombre para el nuevo centro.",
+
+    "Please enter a name for the new synapse.":
+    "Escriba un nombre para la nueva sinapsis.",
+
+    "Please enter a name to create a new synapse for the tag.":
+    "Escriba un nombre para crear una nueva sinapsis para la etiqueta.",
+
+    "Please, log in to like or favourite posts.":
+    "Por favor, entre para marcar los artículos como favoritos.",
+
+    "Projects":
+    "Proyectos",
+
+    "Published articles":
+    "Artículos publicados",
+
+    "Published this month":
+    "Publicados este mes",
+
+    "Published this week":
+    "Publicados esta semana",
+
+    "Published this year":
+    "Publicados este año",
+
+    "Published today":
+    "Publicados hoy",
+
+    "Purple":
+    "Púrpura",
+
+    "Red":
+    "Rojo",
+
+    "Schools":
+    "Centros",
+
+    "Share by Email":
+    "Compartir por correo electrónico",
+
+    "Share on Facebook":
+    "Compartir en Facebook",
+
+    "Share on Twitter":
+    "Compartir en Twitter",
+
+    "Synapse editor":
+    "Edición de sinapsis",
+
+    "Synapses":
+    "Sinapsis",
+
+    "Table of contents":
+    "Tabla de contenidos",
+
+    "Tags":
+    "Etiquetas",
+
+    "Tangerine":
+    "Mandarina",
+
+    "Taxonomies":
+    "Taxonomías",
+
+    "Territories":
+    "Territorios",
+
+    "The comment «{name}» has been deleted.":
+    "El comentario «{name}» ha sido eliminado.",
+
+    "The project «<strong>{name}</strong>» has been deleted.":
+    "El proyecto «<strong>{name}</strong>» ha sido eliminado.",
+
+    "The school «<strong>{name}</strong>» has been deleted.":
+    "El centro «<strong>{name}</strong>» ha sido eliminada.",
+
+    "The synapse «<strong>{name}</strong>» has been deleted.":
+    "La sinapsi «<strong>{name}</strong>» ha sido eliminada.",
+
+    "The tag «<strong>{name}</strong>» has been deleted.":
+    "La etiqueta «<strong>{name}</strong>» ha sido eliminada.",
+
+    "The tag «<strong>{name}</strong>» has been restored.":
+    "La etiqueta «<strong>{name}</strong>» ha sido restaurada.",
+
+    "The user «<strong>{name}</strong>» has been re-enabled.":
+    "El usuario «<strong>{name}</strong>» se ha vuelto a habilitar.",
+
+    "The user «<strong>{name}</strong>» has been successfully disabled.":
+    "El usuario «<strong>{name}</strong>» ha sido desactivado con éxito.",
+
+    "This action cannot be undone.":
+    "Esta acción no puede deshacerse.",
+
+    "To edit the tag, a synapse must be created.":
+    "Para editar la etiqueta, primero debe crearse una sinapsis.",
+
+    "Type to search":
+    "Escriba para buscar",
+
+    "Untitled block":
+    "Bloque sin título",
+
+    "Web syndication":
+    "Sindicación web",
+
+    "White":
+    "Blanco",
+
+    "Yellow":
+    "Amarillo",
+
+    "Your account has been temporarily disabled.":
+    "Tu cuenta se inhabilitó temporalmente."
 };
