2e2b9469c46c01fa98ec029a8cd7bf51519e47c4
Joan Sala <jsala236@xtec.cat>
2018-05-07 10:16:29 +0200

    Applies sort and with queries for the synapse's filters

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -236,11 +236,15 @@ class Post extends FoundationModel {
     public function scopeForSynapse($query, $synapse) {
         if (!empty($synapse->filters)) {
             if (is_array($synapse->filters)) {
-                $filters = array_filter($synapse->filters, function($v) {
-                    return is_null($v) === false;
-                });
+                $request = new Request(array_filter(
+                    $synapse->filters, function($v) {
+                        return is_null($v) === false;
+                    })
+                );
 
-                $query->filter(new Request($filters));
+                $query->filter($request);
+                $query->include($request);
+                $query->sort($request);
             }
         }
         
