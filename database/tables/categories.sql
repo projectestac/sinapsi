f8f97419cc67cdbc0a269ee20a4deb170260da91
Joan Sala <jsala236@xtec.cat>
2019-02-18 15:50:54 +0100

    Database support for categories and hierarchical territories

--- database/tables/categories.sql
+++ database/tables/categories.sql
@@ -1,15 +1,12 @@
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `territories` (
+CREATE TABLE `categories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `uuid` varchar(36) DEFAULT NULL,
-  `territory_id` int(10) unsigned DEFAULT NULL COMMENT 'Parent territory',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `territories_uuid_unique` (`uuid`),
-  KEY `territories_territory_id_foreign_idx` (`territory_id`),
-  CONSTRAINT `territories_territory_id_foreign` FOREIGN KEY (`territory_id`) REFERENCES `territories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+  UNIQUE KEY `territories_uuid_unique` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
